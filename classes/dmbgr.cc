43ddafedd43b4afee57aff30ea1501720efa64e7
ThrudTheBarbarian <github@gornall.net>
2023-12-29 21:56:28 -0800

    Altered the db structure a little to make modules more prominent

--- classes/dmbgr.cc
+++ classes/dmbgr.cc
@@ -115,7 +115,8 @@ void DbMgr::_createInitialSchema(void)
 					"id      INTEGER PRIMARY KEY,\n"
 					"name    VARCHAR(128) NOT NULL DEFAULT 'unnamed',\n"
 					"module  INTEGER NOT NULL DEFAULT -1,\n"
-					"driver  VARCHAR(128) NOT NULL DEFAULT 'unknown'\n"
+					"driver  VARCHAR(128) NOT NULL DEFAULT 'unknown',\n"
+					"render  VARCHAR(128) NOT NULL DEFAULT 'unknown'\n"
 					")\n"))
 		ERR << "Cannot create inputs table";
 
@@ -124,9 +125,20 @@ void DbMgr::_createInitialSchema(void)
 					"id      INTEGER PRIMARY KEY,\n"
 					"name    VARCHAR(128) NOT NULL DEFAULT 'unnamed',\n"
 					"module  INTEGER NOT NULL DEFAULT -1,\n"
-					"driver  VARCHAR(128) NOT NULL DEFAULT 'unknown'\n"
+					"driver  VARCHAR(128) NOT NULL DEFAULT 'unknown',\n"
+					"render  VARCHAR(128) NOT NULL DEFAULT 'unknown'\n"
 					")\n"))
 		ERR << "Cannot create outputs table";
+
+	if (!query.exec("CREATE TABLE IF NOT EXISTS modules\n"
+					"(\n"
+					"id      INTEGER PRIMARY KEY,\n"
+					"name    VARCHAR(128) NOT NULL DEFAULT 'unnamed',\n"
+					"driver  VARCHAR(128) NOT NULL DEFAULT 'unknown',\n"
+					"nodeId  VARCHAR(32) NOT NULL DEFAULT '',\n"
+					"render  VARCHAR(128) NOT NULL DEFAULT 'unknown'\n"
+					")\n"))
+		ERR << "Cannot create modules table";
 
 	QFile f(Config::instance().databaseDir() + "/reef.sql");
 	if (f.open(QFile::ReadOnly))
@@ -154,14 +166,34 @@ void DbMgr::fetchSystemInfo(QString user, QString identifier)
 	QSqlDatabase db = QSqlDatabase::database();
 
 	/**************************************************************************\
-	|* Add the list of known inputs to the results
+	|* Add the list of known modules to the results
 	\**************************************************************************/
 	QSqlQuery query;
-	query.exec("SELECT id, name, module, driver "
-			   "FROM inputs "
+	query.exec("SELECT id, name, nodeId, driver, render "
+			   "FROM modules "
 			   "ORDER BY name");
 
 	QJsonObject records;
+	QJsonArray modules;
+	QJsonObject module;
+	while (query.next())
+		{
+		module.insert("id", QJsonValue::fromVariant(query.value(0)));
+		module.insert("name", QJsonValue::fromVariant(query.value(1)));
+		module.insert("nodeId", QJsonValue::fromVariant(query.value(2)));
+		module.insert("driver", QJsonValue::fromVariant(query.value(3)));
+		module.insert("renderer", QJsonValue::fromVariant(query.value(4)));
+		modules.push_back(module);
+		}
+	records.insert("modules", modules);
+
+	/**************************************************************************\
+	|* Add the list of known inputs to the results
+	\**************************************************************************/
+	query.exec("SELECT id, name, module, driver, render "
+			   "FROM inputs "
+			   "ORDER BY name");
+
 	QJsonArray inputs;
 	QJsonObject input;
 	while (query.next())
@@ -170,6 +202,7 @@ void DbMgr::fetchSystemInfo(QString user, QString identifier)
 		input.insert("name", QJsonValue::fromVariant(query.value(1)));
 		input.insert("module", QJsonValue::fromVariant(query.value(2)));
 		input.insert("driver", QJsonValue::fromVariant(query.value(3)));
+		input.insert("renderer", QJsonValue::fromVariant(query.value(4)));
 		inputs.push_back(input);
 		}
 	records.insert("inputs", inputs);
@@ -177,7 +210,7 @@ void DbMgr::fetchSystemInfo(QString user, QString identifier)
 	/**************************************************************************\
 	|* Add the list of known outputs to the results
 	\**************************************************************************/
-	query.exec("SELECT id, name, module, driver "
+	query.exec("SELECT id, name, module, driver, render "
 			   "FROM outputs "
 			   "ORDER BY name");
 
@@ -189,9 +222,16 @@ void DbMgr::fetchSystemInfo(QString user, QString identifier)
 		output.insert("name", QJsonValue::fromVariant(query.value(1)));
 		output.insert("module", QJsonValue::fromVariant(query.value(2)));
 		output.insert("driver", QJsonValue::fromVariant(query.value(3)));
+		output.insert("renderer", QJsonValue::fromVariant(query.value(4)));
 		outputs.push_back(output);
 		}
 	records.insert("outputs", outputs);
+
+	/**************************************************************************\
+	|* Add the name of this return-type
+	\**************************************************************************/
+	QJsonValue methodName = "SysInfo";
+	records.insert("method", methodName);
 
 	/**************************************************************************\
 	|* Create the JSON record and send to the client who called us
